238314ae775da4119954f5123633c4863ec4bc05
Thanutheepa <thanujamahendran@gmail.com>
2022-09-05 09:03:31 +0530

    Stylying part

---
+++ BlazorServerApp/Data/Bool.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+
+//namespace BlazorServerApp.Data
+//{
+   // class Bool
+    //{
+        //static void Main(string[] args)
+        //{
+           // bool type = true;
+
+            //if (type)
+            //{
+               // Console.WriteLine("Show the type");
+            //}
+            //else
+            //{
+                //Console.WriteLine("Hide the type");
+            //}
+
+            //Console.WriteLine("AddOutlets, UserCreation, UserProfile are commented");
+        //}
+    //}
+//}
